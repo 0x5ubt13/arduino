addc666b59cb50dd00fc16ee108e970172ad8fcb
0x5ubt13 <5ubt13@protonmail.com>
2024-01-06 17:40:27 +0000

    dulce navidad

--- hardware_kit_tutorial/My sketches/2_1.ino
+++ hardware_kit_tutorial/My sketches/2_1.ino
@@ -1,7 +1,73 @@
-void blink() {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+void setup(){
+  pinMode(13, OUTPUT);
 }
 
+void loop(){
+  blink();
+}
+
+void blink() {
+    // la la la
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+    
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+    
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+    
+    delay(750);
+    
+    // la la la
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+    
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+    
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+    
+    delay(750);
+    
+    // la la la, lala
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+    
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+    
+    digitalWrite(13, HIGH);
+    delay(250);
+    digitalWrite(13, LOW);
+    delay(500);
+    
+    digitalWrite(13, HIGH);
+    delay(150);
+    digitalWrite(13, LOW);
+    delay(150);
+    
+    digitalWrite(13, HIGH);
+    delay(150);
+    digitalWrite(13, LOW);
+    delay(150);
+    
+    delay(3000);
+}
